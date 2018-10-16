4115afe252eacfa4c3301d4543ced028a6551771
Your Name <you@example.com>
2018-10-16 17:41:55 +0800

    实现注册功能

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 var express = require('express');
 var nunjucks = require('nunjucks');
+var mysql = require('mysql');
+var moment = require('moment');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 var app = express();
+
+
+var connection = mysql.createConnection({
+    host: '192.168.0.110',
+    user: 'root',
+    password: '88888888',
+    database: 'okr'
+})
+
+connection.connect();
 
 nunjucks.configure('views', {
     autoescape: true,
@@ -8,9 +22,34 @@ nunjucks.configure('views', {
     express: app,
 });
 
-app.get('/personal',function(req,res){
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.get('/personal', function (req, res) {
     res.render('personal.html')
 });
+
+app.get('/homepage', function (req, res) {
+
+    connection.query(`select * from user`, function (err, data) {
+        console.log('data:', data)
+        res.render('homepage.html', { items: data })
+    })
+
+});
+
+app.post('/api/homepage', function (req, res) {
+    var phone = req.body.phone;
+    var password = req.body.password;
+    var token = req.body.token;
+    var created_at = moment().format('YYYY-MM-DD HH:mm:ss');
+
+    console.log(phone,token)
+
+    connection.query('insert into user values (null, ? , ? , "" , "" , ?, ?)', [ phone, password , token ,created_at], function (err, data) {
+        res.send('注册成功')
+    })
+})
 
 
 app.listen(3000)
